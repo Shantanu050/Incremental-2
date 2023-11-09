20ed7a8fd081e946cf6303eb9b67db13d572cefe
Iamneo <techadmin@iamneo.ai>
2023-11-09 10:03:29 +0000

    Backup Commit

--- dotnetapp/Controllers/PlayerController.cs
+++ dotnetapp/Controllers/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
+// using Microsoft.EntityFrameworkCore.SqlServer;
 
 using dotnetapp.Models;
 
